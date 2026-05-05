21f8eddf1bc226f4f37baf65c8792f343272dbc9
Wadahan <Wadahan@hotmail.com>
2026-05-05 14:26:17 -0300

    Update 08 - Create VitrineFiltroViewModel

---
+++ src/Doalim-dev/ViewModels/VitrineFiltroViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Doalim_dev.ViewModels
+{
+    public class VitrineFiltroViewModel
+    {
+        public int? QuantidadeMinima { get; set; }
+        public string OrdemValidade { get; set; } = "asc";
+        public string? NomeBusca { get; set; }
+
+    }
+}
