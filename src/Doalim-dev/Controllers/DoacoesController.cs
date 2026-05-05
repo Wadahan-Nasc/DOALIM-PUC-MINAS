fa824ace471986dd78add96adc679faa03985b61
Wadahan <Wadahan@hotmail.com>
2026-05-05 14:48:01 -0300

    Update 10 - Atualização DoacoesController

--- src/Doalim-dev/Controllers/DoacoesController.cs
+++ src/Doalim-dev/Controllers/DoacoesController.cs
@@ -15,27 +15,49 @@ namespace Doalim_dev.Controllers
         }
 
         // GET: Doacoes
-        public async Task<IActionResult> Vitrine()
+        public async Task<IActionResult> Vitrine(VitrineFiltroViewModel filtros)
         {
-            var doacoes = await _context.Doacoes
+            var query = _context.Doacoes
                 .Include(a => a.Doador)
-                .ThenInclude(d => d.Usuario) // Inclui os dados do usuário do doador
+                    .ThenInclude(d => d.Usuario) // Inclui os dados do usuário do doador
                 .Where(a => a.StatusProduto && a.DataValidade > DateTime.UtcNow && a.QuantidadeDisponivel > 0)
-                .OrderByDescending(a => a.DataCadastro)
+                .AsQueryable();
+
+            // Filtro de quantidade mínima
+            if (filtros.QuantidadeMinima.HasValue)
+                query = query.Where(a => a.QuantidadeDisponivel >= filtros.QuantidadeMinima.Value);
+
+            //Filtro de busca por nome
+            if (!string.IsNullOrWhiteSpace(filtros.NomeBusca))
+                query = query.Where(a => a.Nome.Contains(filtros.NomeBusca));
+
+            //Filtro de ordenação por validade
+            query = filtros.OrdemValidade == "desc"
+                ? query.OrderByDescending(a => a.DataValidade)
+                : query.OrderBy(a => a.DataValidade);
+
+            var produtos = await query
                 .Select(a => new VitrineDoacoesViewModel
                 {
                     IdProduto = a.IdProduto,
                     Nome = a.Nome,
-                    Categoria = a.Categoria,
-                    MarcaProduto = a.MarcaProduto,
-                    TipoArmazenamento = a.TipoArmazenamento,
                     DataValidade = a.DataValidade,
+                    Categoria = a.Categoria ?? "",
+                    MarcaProduto = a.MarcaProduto ?? "",
+                    TipoArmazenamento = a.TipoArmazenamento ?? "",
+                    FotoProduto = a.FotoProduto ?? "",
                     QuantidadeDisponivel = a.QuantidadeDisponivel,
-                    NomeDoador = a.Doador.Usuario.Nome,
-                    FotoProduto = a.FotoProduto
+                    NomeDoador = a.Doador.Usuario.Nome
                 })
                 .ToListAsync();
-            return View(doacoes);
+
+            var viewModel = new VitrineCompletaViewModel
+            {
+                Filtros = filtros,
+                Produtos = produtos
+            };
+
+            return View(viewModel);
         }
     }
 }
