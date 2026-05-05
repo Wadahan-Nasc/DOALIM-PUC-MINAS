9e8ed43d2e2a389fef6600a85d7e2760e399af17
Wadahan <Wadahan@hotmail.com>
2026-05-05 14:31:21 -0300

    Update 09 - Create VitrineCompletaViewModel

---
+++ src/Doalim-dev/ViewModels/VitrineCompletaViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Doalim_dev.ViewModels
+{
+    public class VitrineCompletaViewModel
+    {
+        public VitrineFiltroViewModel Filtros { get; set; } = new();
+        public IEnumerable<VitrineDoacoesViewModel> Produtos { get; set; } = new List<VitrineDoacoesViewModel>();
+    }
+}
