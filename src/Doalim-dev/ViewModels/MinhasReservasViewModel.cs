3ac2f44148204b1c4cd0eb4e81ed5d1ebd29754a
Wadahan Nascimento <wadahan@hotmail.com>
2026-05-24 12:32:51 -0300

    Atualuização da ViewModel de MinhasReservas para receber as informações dos lotes

--- src/Doalim-dev/ViewModels/MinhasReservasViewModel.cs
+++ src/Doalim-dev/ViewModels/MinhasReservasViewModel.cs
@@ -7,12 +7,15 @@
         public string StatusReserva { get; set; }
         public int QuantidadeReservada { get; set; }
 
+        // Dados do lote
+        public string NumeroLote { get; set; }
+        public DateTime DataValidadeLote { get; set; }
+
         // Dados do produto
         public string NomeProduto { get; set; }
         public string MarcaProduto { get; set; }
         public string Categoria { get; set; }
         public string UnidadeMedida { get; set; }
-        public DateTime? DataValidade { get; set; }
         public string? FotoProduto { get; set; } // Base64 convertido no controller
 
         // Dados do doador
